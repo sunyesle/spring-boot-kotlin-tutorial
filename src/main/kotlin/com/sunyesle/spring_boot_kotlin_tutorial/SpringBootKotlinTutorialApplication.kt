e85dd03be6509c61faae1f1d567b9716529d101b
sunyesle <sunyesle@gmail.com>
2025-04-07 21:48:04 +0900

    init

---
+++ src/main/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/SpringBootKotlinTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.sunyesle.spring_boot_kotlin_tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootKotlinTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/SpringBootKotlinTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sunyesle.spring_boot_kotlin_tutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
