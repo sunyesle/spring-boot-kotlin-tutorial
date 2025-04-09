a1c811596f5dd7b0931c84603e8819b3cf811175
sunyesle <sunyesle@gmail.com>
2025-04-09 20:52:26 +0900

    Mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test"){
+		exclude(module = "mockito-core")
+	}
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
