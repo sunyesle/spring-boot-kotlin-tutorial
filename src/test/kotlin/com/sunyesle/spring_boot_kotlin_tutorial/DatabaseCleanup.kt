4eaacafa5169dd1a0a3409857a9235d87dcb36ff
sunyesle <sunyesle@gmail.com>
2025-04-13 02:30:33 +0900

    테스트 데이터 초기화를 위한 클래스 추가

---
+++ src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/DatabaseCleanup.kt
@@ -0,0 +1,37 @@
+package com.sunyesle.spring_boot_kotlin_tutorial
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import jakarta.transaction.Transactional
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.stereotype.Component
+
+@Component
+class DatabaseCleanup : InitializingBean {
+
+    @PersistenceContext
+    private lateinit var entityManager: EntityManager
+
+    private lateinit var tableNames: List<String>
+
+    override fun afterPropertiesSet() {
+        tableNames = entityManager.metamodel.entities
+            .filter { it.javaType.getAnnotation(Entity::class.java) != null }
+            .map { it.name.toSnakeCase() }
+            .toList()
+    }
+
+    @Transactional
+    fun execute() {
+        entityManager.flush()
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate()
+        for (tableName in tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE \"$tableName\"").executeUpdate()
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate()
+    }
+
+    private fun String.toSnakeCase(): String =
+        replace(Regex("([a-z])([A-Z])"), "$1_$2").lowercase()
+}
