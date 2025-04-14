c324fcb68b23d1cb6a1ca07633bb407c1303ca78
sunyesle <sunyesle@gmail.com>
2025-04-14 22:35:55 +0900

    Kotest 설정 Spring extension 등록

---
+++ src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/ProjectConfig.kt
@@ -0,0 +1,8 @@
+package com.sunyesle.spring_boot_kotlin_tutorial
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.extensions.spring.SpringExtension
+
+class ProjectConfig : AbstractProjectConfig() {
+    override fun extensions() = listOf(SpringExtension)
+}
