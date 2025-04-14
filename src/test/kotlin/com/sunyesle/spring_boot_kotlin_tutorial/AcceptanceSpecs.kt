b338c9328fac0d8d2788335781b79fcbf5ff1468
sunyesle <sunyesle@gmail.com>
2025-04-14 22:45:28 +0900

    Kotest 기반 인수 테스트용 베이스 클래스 추가

---
+++ src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/AcceptanceSpecs.kt
@@ -0,0 +1,39 @@
+package com.sunyesle.spring_boot_kotlin_tutorial
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.restassured.RestAssured
+import io.restassured.builder.RequestSpecBuilder
+import io.restassured.config.LogConfig
+import io.restassured.filter.log.LogDetail
+import io.restassured.http.ContentType
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+abstract class AcceptanceSpecs (body: DescribeSpec.() -> Unit) : DescribeSpec(body) {
+
+    @LocalServerPort
+    var port: Int = 0
+
+    @Autowired
+    lateinit var databaseCleanup: DatabaseCleanup
+
+    init {
+        beforeSpec {
+            RestAssured.port = port
+
+            val logConfig = LogConfig.logConfig()
+                .enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)
+
+            val config = RestAssured.config().logConfig(logConfig)
+
+            RestAssured.requestSpecification = RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setConfig(config)
+                .build()
+
+            databaseCleanup.execute()
+        }
+    }
+}
