f93c1eb3c59d6e32f1d108a1cf08c996ac3aead7
sunyesle <sunyesle@gmail.com>
2025-05-11 22:33:42 +0900

    Article 저장 API 테스트 추가

--- src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/kotest/ArticleAcceptanceTest.kt
+++ src/test/kotlin/com/sunyesle/spring_boot_kotlin_tutorial/kotest/ArticleAcceptanceTest.kt
@@ -1,17 +1,41 @@
 package com.sunyesle.spring_boot_kotlin_tutorial.kotest
 
+import com.sunyesle.spring_boot_kotlin_tutorial.article.ArticleRequest
 import com.sunyesle.spring_boot_kotlin_tutorial.common.AcceptanceTest
+import com.sunyesle.spring_boot_kotlin_tutorial.security.TokenRequest
 import io.kotest.core.spec.style.FunSpec
+import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
-import org.hamcrest.Matchers.equalTo
-import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.*
 import org.springframework.test.context.jdbc.Sql
 
 @AcceptanceTest
 @Sql(scripts = ["classpath:acceptance/article.sql"])
 class ArticleAcceptanceTest : FunSpec({
+
+    context("글 저장") {
+
+        test("유효한 정보를 입력하면 201 응답을 반환한다") {
+            // given
+            val accessToken = generateToken(TokenRequest("johnDoe", "password"))
+            val request = ArticleRequest("new article", "test headline", "test content")
+
+            // when
+            Given{
+                header("Authorization", "Bearer $accessToken")
+                body(request)
+                log().all()
+            } When {
+                post("/api/article")
+            } Then {
+                statusCode(201)
+                body("id", notNullValue())
+                log().all()
+            }
+        }
+    }
 
     context("글 목록 조회") {
 
@@ -40,3 +64,13 @@ class ArticleAcceptanceTest : FunSpec({
         }
     }
 })
+
+private fun generateToken(request: TokenRequest): String {
+    return Given {
+        body(request)
+    } When {
+        post("/api/auth/token")
+    } Extract {
+        path("accessToken")
+    }
+}
